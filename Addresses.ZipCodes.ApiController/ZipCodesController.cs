b6dde535751bca898536a3b8469263e24ed68820
Christopher Dorst <chris@dorst.email>
2018-03-29 12:18:10 -0500

    Update ZipCodesController.cs

--- Addresses.ZipCodes.ApiController/ZipCodesController.cs
+++ Addresses.ZipCodes.ApiController/ZipCodesController.cs
@@ -4,6 +4,7 @@
 using Addresses.ZipCodes.DatabaseContext;
 using DevOps.Code.DataAccess.Interfaces.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace Addresses.ZipCodes.ApiController
